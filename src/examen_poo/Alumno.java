b8bc7ed3554a7346cb28c23b384acbb436d712ea
Deymi <n00443762@upn.pe>
2026-09-21 21:08:35 -0500

    se agrego validaciones

--- src/examen_poo/Alumno.java
+++ src/examen_poo/Alumno.java
@@ -17,9 +17,89 @@ public class Alumno {
 
     public Alumno() {
     }
-    
-    
 
+    public Alumno(String nombre, String tipodocumento, String numerodocumento, char nivelsocioeconomico, String tipobeca) {
+        this.nombre = nombre;
+        this.tipodocumento = tipodocumento;
+        this.numerodocumento = numerodocumento;
+        this.nivelsocioeconomico = nivelsocioeconomico;
+        this.tipobeca = tipobeca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean setNombre(String nombre) {
+        if(nombre.length()>0){
+            this.nombre = nombre;
+            return true;
+        }else{
+            System.out.println("el nombre no puede estar vacio");
+            return false;
+        }   
+
+    }
+
+    public String getTipodocumento() {
+        return tipodocumento;
+    }
+
+    public boolean setTipodocumento(String tipodocumento) {
+        if(tipodocumento.equals("DNI")||tipodocumento.equals("carnet de rsidencia")){
+            this.tipodocumento = tipodocumento;
+            return true;
+        }else{
+            System.out.println("tipo de documento invalido");
+            return true;
+        }      
+    }
+
+
+    public String getNumerodocumento() {
+        return numerodocumento;
+    }
+
+    public boolean setNumerodocumento(String numerodocumento) {
+         if(numerodocumento.length() == 8){
+            this.numerodocumento = numerodocumento;
+            return true;
+        }else{
+            System.out.println("ERROR cantidad de caracteres inválido"); 
+            return false;
+        }
+    }
+
+
+    public char getNivelsocioeconomico() {
+        return nivelsocioeconomico;
+    }
+
+    public boolean setNivelsocioeconomico(char nivelsocioeconomico) {
+        char auxillar = Character.toUpperCase(nivelsocioeconomico);
+        if(auxillar=='A' || auxillar=='B'|| auxillar=='C'){
+            this.nivelsocioeconomico=auxillar;
+            return true;
+        }else{
+            System.out.println("Error nivel socioeconomico invalido. solo A, B, C");
+            return false;
+        }
+    }
+
+
+    public String getTipobeca() {
+        return tipobeca;
+    }
+
+    public boolean setTipobeca(String tipobeca) {
+        if(tipobeca.equals("ninguna")||tipobeca.equals("parcial")||tipobeca.equals("total")){
+            this.tipobeca = tipobeca;
+            return true;
+        }else{
+            System.out.println("Error. eliga un  tipo de beca valido. ninguna, parcial, total");
+            return false;
+        }
+    }
 }
 
     
