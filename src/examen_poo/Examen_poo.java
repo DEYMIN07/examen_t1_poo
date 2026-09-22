fe07bb463095fefc108657fc2a8de28321fbdd20
Deymi <n00443762@upn.pe>
2026-09-21 21:24:22 -0500

    SE AGREGO EL MAIN

--- src/examen_poo/Examen_poo.java
+++ src/examen_poo/Examen_poo.java
@@ -4,6 +4,8 @@
  */
 package examen_poo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author deymi
@@ -15,6 +17,29 @@ public class Examen_poo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new  Scanner(System.in);
+          Alumno a1 = new Alumno();
+         
+         System.out.print("Ingresa el nombre del alumno: ");
+         boolean aux = a1.setNombre(sc.nextLine());
+         while (aux == false) {
+             System.out.print("Vuelve a ingresar el nombre: ");
+             aux = a1.setNombre(sc.nextLine());
+         }
+         
+         System.out.print("Ingresa tipo de documento (DNI / CarnéResidencia): ");
+         aux = a1.setTipodocumento(sc.nextLine());
+         while (aux == false) {
+             System.out.print("Vuelve a ingresar tipo de documento: ");
+             aux = a1.setTipodocumento(sc.nextLine());
+         }
+
+         System.out.print("Ingresa número de documento: ");
+         aux = a1.setNumerodocumento(sc.nextLine());
+         while (aux == false) {
+             System.out.print("Vuelve a ingresar número: ");
+             aux = a1.setNumerodocumento(sc.nextLine());
+         }
     }
     
 }
