0f7761efdde4274c22f2c9e710224fad481de412
Deymi <n00443762@upn.pe>
2026-09-21 21:01:08 -0500

    creacion de clase alumno y sus atributos

---
+++ src/examen_poo/Alumno.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen_poo;
+
+/**
+ *
+ * @author deymi
+ */
+public class Alumno {
+    private String nombre;
+    private String tipodocumento;
+    private String numerodocumento;
+    private char nivelsocioeconomico;
+    private String tipobeca;
+
+}
